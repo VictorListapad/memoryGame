13e75dcf2a2e83c53af9c8b0f54738c4aaaa2072
Victor Listapad <victorlistapadjob@gmail.com>
2022-03-03 20:03:00 -0500

    created flip card logicand logic to check if cards match

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ const cards = [
   },
 ];
 const board = document.getElementById(`board`);
+const score = document.getElementById(`score`);
+let chosenCards = [];
+let chosenCardsIds = [];
+let cardsWon = [];
 // ************************************************************************
 // CREATE BOARD
 // ************************************************************************
@@ -68,4 +72,40 @@ function createBoard() {
 createBoard();
 
 // CARD FLIP
-function flipCard() {}
+function flipCard() {
+  const cardId = this.dataset.id;
+  console.log(cardId);
+  this.setAttribute(`src`, cards[cardId].img);
+  chosenCardsIds.push(cardId);
+  chosenCards.push(cards[cardId].name);
+  if (chosenCards.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
+
+// CHECK MATCH
+function checkMatch() {
+  const allCards = document.querySelectorAll(`#board img`);
+  console.log(allCards);
+  const firstChosenId = chosenCardsIds[0];
+  const secondsChosenId = chosenCardsIds[1];
+  if (firstChosenId !== secondsChosenId && chosenCards[0] === chosenCards[1]) {
+    score.innerText = Number(score.innerText) + 2;
+    allCards[firstChosenId].setAttribute(`src`, `./images/white.png`);
+    allCards[secondsChosenId].setAttribute(`src`, `./images/white.png`);
+    cardsWon.push(chosenCards[0]);
+    cardsWon.push(chosenCards[1]);
+    allCards[firstChosenId].removeEventListener(`click`, flipCard);
+    allCards[secondsChosenId].removeEventListener(`click`, flipCard);
+  } else {
+    allCards[firstChosenId].setAttribute(`src`, `./images/blank.png`);
+    allCards[secondsChosenId].setAttribute(`src`, `./images/blank.png`);
+  }
+  chosenCards = [];
+  chosenCardsIds = [];
+  if (cardsWon.length === 12) {
+    cardsWon = [];
+    allCards.forEach((card) => card.remove());
+    createBoard();
+  }
+}
